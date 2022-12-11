658275075f199a549f4ee77be83b019bdc43e750
Khandaker Nabiul <na.ninad60@gmail.com>
2022-12-12 02:58:59 +0600

    working with Comment a post API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require("express-validator");
 const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
 const User = require("../../models/Users");
+const Users = require("../../models/Users");
 
 // @route   POST api/posts
 // @desc    Create a post
@@ -152,5 +153,40 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/posts/comment/:id
+// @desc    Add comment in a post
+// @access  Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.id);
+      const user = await Users.findById(req.user.id).select("-password");
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
